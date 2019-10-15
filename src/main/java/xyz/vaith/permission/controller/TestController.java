0b4566f5e65a7320b353be4c190cd3b01320d1b1
Vaith <vaithwee@yeah.net>
2019-10-15 23:23:57 +0800

    Add test example

--- src/main/java/xyz/vaith/permission/controller/TestController.java
+++ src/main/java/xyz/vaith/permission/controller/TestController.java
@@ -1,5 +1,8 @@
 package xyz.vaith.permission.controller;
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +10,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequestMapping("/test")
 @Controller
+@Slf4j
 public class TestController {
     @RequestMapping("/hello")
     @ResponseBody
     public String hello() {
+        log.info("hello permission");
         return "hello, permission";
     }
 }
